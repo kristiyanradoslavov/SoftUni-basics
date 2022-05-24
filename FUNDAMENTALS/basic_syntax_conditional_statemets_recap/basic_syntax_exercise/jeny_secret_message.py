c64c0021490166f1dcef557bdf84e4b33760ebca
Kristiyan Radoslavov <radoslavov48@gmail.com>
2022-05-24 13:20:46 +0300

    removing fundamentals from Basics

--- FUNDAMENTALS/basic_syntax_conditional_statemets_recap/basic_syntax_exercise/jeny_secret_message.py
+++
@@ -1,7 +0,0 @@
-input_name = input()
-
-if input_name == "Johnny":
-    print("Hello, my love!")
-else:
-    print(f"Hello, {input_name}!")
-
--- FUNDAMENTALS/basic_syntax_conditional_statemets_recap/basic_syntax_lab/word_reverse.py
+++
@@ -1,8 +0,0 @@
-input_word = input()
-
-reversed_word = ""
-
-for i in range(len(input_word) -1, -1, -1):
-    reversed_word += input_word[i]
-
-print(reversed_word)
